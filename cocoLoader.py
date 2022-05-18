aac27febdca692c501579a40048e305d4c2cf341
shivaank agarwal <shivaankagarwal@shivaanks-MacBook-Air.local>
2022-05-18 12:38:41 -0700

    Added Bluring in COCO dataloader

--- cocoLoader.py
+++ cocoLoader.py
@@ -5,6 +5,8 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import os
 from pycocotools.coco import COCO
+import cv2
+import torch
 
 transform = transforms.Compose([
         transforms.Resize((256, 256)),
@@ -34,9 +36,7 @@ category_dict = {'person': 1,
                  'tv': 72}
 
 class CocoDataset(Dataset):
-    def getBlurredOutput(self, img):
-        return img
-
+    
     def __init__(self, rootDir: str, annFile: str, transform = None) -> None:
         super(CocoDataset, self).__init__()
         self.coco = COCO(annFile)
@@ -53,13 +53,67 @@ class CocoDataset(Dataset):
         print(len(self.ids))
         self.root = rootDir
         self.transform = transform
+        self.blur_transform = transforms.Compose([transforms.ToTensor(),transforms.Resize((256, 256))])
+        
+    def motion_blur_horizontal(self,img_path,kernel_size):
+        img = cv2.imread(img_path)
+
+        kernel_h = np.zeros((kernel_size, kernel_size))
+        kernel_h[int((kernel_size - 1)/2), :] = np.ones(kernel_size)
+        kernel_h /= kernel_size
+
+        horizontal_mb = cv2.filter2D(img, -1, kernel_h)
+        horizontal_mb = cv2.cvtColor(horizontal_mb, cv2.COLOR_RGB2BGR)
+        return horizontal_mb
+    
+    def motion_blur_vertical(self,img_path,kernel_size):
+        img = cv2.imread(img_path)
+
+        kernel_v = np.zeros((kernel_size, kernel_size))
+        kernel_v[:, int((kernel_size - 1)/2)] = np.ones(kernel_size)
+        kernel_v /= kernel_size
+
+        vertical_mb = cv2.filter2D(img, -1, kernel_v)
+        vertical_mb = cv2.cvtColor(vertical_mb, cv2.COLOR_RGB2BGR)
+        return vertical_mb
+    
+    def gaussian_blur(self,img_path,k_size):
+        img = cv2.imread(img_path)
+        img = cv2.GaussianBlur(img,(k_size,k_size),0)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        return img
+    
+    def avg_blur(self,img_path,k_size):
+        img = cv2.imread(img_path)
+        img = cv2.blur(img, (k_size,k_size))
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        return img
+        
+    def getBlurredOutput(self,img_path):
+        img_path = os.path.join(self.root, img_path)
+        p = torch.rand(1)
+        k_size = 15
+        
+        if p<=0.25:
+            #print("Vertical")
+            return self.motion_blur_vertical(img_path,k_size)
+        if p<=0.5:
+            #print("Horizontal")
+            return self.motion_blur_horizontal(img_path,k_size)
+        if p<=0.75:
+            #print("Average")
+            return self.avg_blur(img_path,k_size)
+        else:
+            #print("Gaussian")
+            return self.gaussian_blur(img_path,k_size)
+     
 
     def __len__(self) -> int:
         return len(self.ids)
 
     def _load_image(self, ids: int) -> Image.Image:
         path = self.coco.loadImgs(ids)[0]["file_name"]
-        return Image.open(os.path.join(self.root, path)).convert("RGB")
+        return Image.open(os.path.join(self.root, path)).convert("RGB"),path
 
     def _load_target(self, ids: int):
         target = self.coco.loadAnns(self.coco.getAnnIds(ids))
@@ -83,13 +137,13 @@ class CocoDataset(Dataset):
 
     def __getitem__(self, index: int):
         ids = self.ids[index]
-        image = self._load_image(ids)
-        blurredImage = self.getBlurredOutput(image)
+        image,path = self._load_image(ids)
+        blurredImage = self.getBlurredOutput(path)
         target = self._load_target(ids)
 
         if(self.transform is not None):
             image = self.transform(image)
-            blurredImage = self.transform(blurredImage)
+            blurredImage = self.blur_transform(blurredImage)
 
         return {'image': image, 'inputImg': blurredImage, 'class': target}
 
